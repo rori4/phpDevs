3dfc62164e29e016ac5498587bb3d279b2a64acd
Rangel <RangelStoilov@students.softuni.bg>
2016-12-14 17:42:51 +0200

    test commit

--- views/users/login.php
+++ views/users/login.php
@@ -6,4 +6,5 @@
     <div>Username: <input type="text" name="username" /></div>
     <div>Password: <input type="password" name="password" /></div>
     <div><input type="submit" value="Login"></div>
+    //TODO: test the gitHub
 </form>
