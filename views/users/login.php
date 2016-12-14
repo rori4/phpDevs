00177a02b0a08b0497ae33fc8dd5534984b72bc3
nstoilov99 <nstoilov99@gmail.com>
2016-12-14 17:14:57 +0200

    Commit test

--- views/users/login.php
+++ views/users/login.php
@@ -4,6 +4,6 @@
 
 <form method="post">
     <div>Username: <input type="text" name="username" /></div>
-    <div>Password: <input type="password" name="password" /></div>
+    <div>Password: <input type="text" name="password" /></div>
     <div><input type="submit" value="Login"></div>
 </form>
