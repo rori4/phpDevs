43f15cd2c76aee852234930289266f5f9dbb23ca
nstoilov99 <nstoilov99@gmail.com>
2016-12-14 17:45:26 +0200

    Commit test

--- views/users/login.php
+++ views/users/login.php
@@ -3,6 +3,7 @@
 <h1><?= htmlspecialchars($this->title) ?></h1>
 
 <form method="post">
+    //TODO: test ommit
     <div>Username: <input type="text" name="username" /></div>
     <div>Password: <input type="password" name="password" /></div>
     <div><input type="submit" value="Login"></div>
