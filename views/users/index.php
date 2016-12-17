2f827d98b830c76b3832f03c3c276971b0f13111
Pec <peter.bratanov@gmail.com>
2016-12-17 22:13:00 +0200

    user table update and css stuff

--- views/users/index.php
+++ views/users/index.php
@@ -1,27 +1,37 @@
-<?php $this->title = 'Users'; ?>
+
 
 <h1><?=htmlspecialchars($this->title)?></h1>
 
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Username</th>
-        <th>Full Name</th>
-        <?php if ($_SESSION['user_role']=="admin"):?>
-            <th>Actions</th>
-        <?php endif; ?>
-    </tr>
-    <?php foreach ($this->users as $user) : ?>
+<div class="container">
+    <h2>Users</h2>
+    <table class="table table-striped">
+        <thead>
         <tr>
+            <th>ID</th>
+            <th>Username</th>
+            <th>Fullname</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <?php if ($_SESSION['user_role']=="admin"):?>
+
+        <?php endif; ?>
+        <tbody>
+        <tr>
+            <?php foreach ($this->users as $user) : ?>
             <td><?=$user['id']?></td>
             <td><?=htmlspecialchars($user['username']) ?></td>
             <td><?=htmlspecialchars($user['full_name']) ?></td>
             <?php if ($_SESSION['user_role']=="admin"):?>
-                <td>
-                    <a href="<?=APP_ROOT?>/users/edit/<?=htmlspecialchars($user['id'])?>">[Edit]</a>
-                    <a href="<?=APP_ROOT?>/users/delete/<?=htmlspecialchars($user['id'])?>">[Delete]</a>
-                </td>
+            <td>
+                <button type="submit" value="Edit" class="btn btn-primary"><a href="<?=APP_ROOT?>/users/edit/<?=htmlspecialchars($user['id'])?>"><font color="#eee">Edit</font></a></button>
+                <button type="submit" value="Delete" class="btn btn-primary"><a href="<?=APP_ROOT?>/users/delete/<?=htmlspecialchars($user['id'])?>"><font color="#eee">Delete</font></a></button>
+
+            </td>
             <?php endif; ?>
         </tr>
-    <?php endforeach ?>
-</table>
+        <?php endforeach ?>
+
+        </tbody>
+    </table>
+</div>
