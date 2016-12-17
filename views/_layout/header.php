cb611ffd5785cea31f6e333a30f15506d4c6a09f
Pec <peter.bratanov@gmail.com>
2016-12-17 14:46:27 +0200

    nav bar update - 2

--- views/_layout/header.php
+++ views/_layout/header.php
@@ -21,26 +21,6 @@
 </head>
 
 <body>
-<!--<header>-->
-<!--    <a href="--><?//=APP_ROOT?><!--"><img src="--><?//=APP_ROOT?><!--/content/images/phpDevs_logo_transparent.png"></a>-->
-<!--    <a href="--><?//=APP_ROOT?><!--/">Home</a>-->
-<!--    --><?php //if ($this->isLoggedIn) : ?>
-<!--        <a href="--><?//=APP_ROOT?><!--/posts">Posts</a>-->
-<!--        <a href="--><?//=APP_ROOT?><!--/posts/create">Create Post</a>-->
-<!--        <a href="--><?//=APP_ROOT?><!--/users">Users</a>-->
-<!--    --><?php //else: ?>
-<!--        <a href="--><?//=APP_ROOT?><!--/users/login">Login</a>-->
-<!--        <a href="--><?//=APP_ROOT?><!--/users/register">Register</a>-->
-<!--    --><?php //endif; ?>
-<!--    --><?php //if ($this->isLoggedIn) : ?>
-<!--        <div id="logged-in-info">-->
-<!--            <span>Hello, <b>--><?//=htmlspecialchars($_SESSION['username'])?><!--</b></span>-->
-<!--            <form method="post" action="--><?//=APP_ROOT?><!--/users/logout">-->
-<!--                <input type="submit" value="Logout"/>-->
-<!--            </form>-->
-<!--        </div>-->
-<!--    --><?php //endif; ?>
-<!--</header>-->
 
 <?php require_once('show-notify-messages.php'); ?>
 <header>
