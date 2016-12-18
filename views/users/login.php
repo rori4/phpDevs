9d05dce1bbe03967c55a360fcfe708f041ed5c85
Pec <peter.bratanov@gmail.com>
2016-12-18 14:24:15 +0200

    updates for retards with low resolution

--- views/users/login.php
+++ views/users/login.php
@@ -12,7 +12,7 @@
     <div class="input-group">
         <span class="input-group-addon"><i class="glyphicon glyphicon-lock logo-small"></i></span>
         <input type="password" name="password" class="form-control"  placeholder="Password">
-    </div>
+    </div><br><br>
     <button type="submit" value="Login" class="btn btn-primary">Login</button>
 </form>
 </div>
