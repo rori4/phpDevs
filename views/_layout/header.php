8ea085c1eb34a0914d73e219fa5c6240d1a905cb
Rangel <RangelStoilov@students.softuni.bg>
2016-12-18 02:28:54 +0200

    fixed divs

--- views/_layout/header.php
+++ views/_layout/header.php
@@ -26,39 +26,38 @@
 <body>
 <header>
 <nav class="navbar navbar-inverse">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <a  class="navbar-brand" href="<?=APP_ROOT?>"><font
-                        size="6"
-                        face="VT323"
-                        color="orange"><?=htmlspecialchars("<?php")?><font color="#9370db">Devs</font>></font> </a>
-                </div>
-                <ul class="nav navbar-nav">
-                    <?php if ($this->isLoggedIn) : ?>
-                    <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown"  href="#"> <font color="orange">  Categories</font><span class="caret"></span></a>
-                        <ul class="dropdown-menu">
-                            <li><a href="<?=APP_ROOT?>/posts"><font color="#9370db">  Pussy</font></a></li>
-                            <li><a href="<?=APP_ROOT?>/posts"><font color="#9370db">  Money</font></a></li>
-                            <li><a href="<?=APP_ROOT?>/posts"><font color="#9370db">  Weed</font></a></li>
-                        </ul>
-                    </li>
-                    <li><a href="<?=APP_ROOT?>/posts"><font color="orange">  Posts</font></a></li>
-                    <li><a href="<?=APP_ROOT?>/posts/create"><font color="orange">  Create Post</font></a></li>
-                    <li><a href="<?=APP_ROOT?>/users"><font color="orange"> Users</font></a></li>
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <a  class="navbar-brand" href="<?=APP_ROOT?>"><font
+                size="6"
+                face="VT323"
+                color="orange"><?=htmlspecialchars("<?php")?><font color="#9370db">Devs</font>></font> </a>
+        </div>
+        <ul class="nav navbar-nav">
+            <?php if ($this->isLoggedIn) : ?>
+            <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown"  href="#"> <font color="orange">  Categories</font><span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="<?=APP_ROOT?>/posts"><font color="#9370db">  Pussy</font></a></li>
+                    <li><a href="<?=APP_ROOT?>/posts"><font color="#9370db">  Money</font></a></li>
+                    <li><a href="<?=APP_ROOT?>/posts"><font color="#9370db">  Weed</font></a></li>
                 </ul>
-                    <?php else: ?>
-                <ul class="nav navbar-nav navbar-right">
-                    <li><a href="<?=APP_ROOT?>/users/login"><span class="glyphicon glyphicon-log-in logo-small"></span><font color="orange">  Login</font></a></li>
-                    <li><a href="<?=APP_ROOT?>/users/register"><span class="glyphicon glyphicon-user logo-small"></span><font color="#9370db"> Sign up</font></font> </a></li>
-                </ul>
-            </div>
-    <?php endif; ?>
-    <?php if ($this->isLoggedIn) : ?>
-    <div id="logged-in-info">
-        <li><a href="<?=APP_ROOT?>/users/logout"><span class="glyphicon glyphicon-log-out logo-small"></span><font color="orange">  Logout</font></a></li>
+            </li>
+            <li><a href="<?=APP_ROOT?>/posts"><font color="orange">  Posts</font></a></li>
+            <li><a href="<?=APP_ROOT?>/posts/create"><font color="orange">  Create Post</font></a></li>
+            <li><a href="<?=APP_ROOT?>/users"><font color="orange"> Users</font></a></li>
+            <?php endif; ?>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+            <?php if ($this->isLoggedIn) : ?>
+            <li><a href="<?=APP_ROOT?>/users/logout"><span class="glyphicon glyphicon-log-out logo-small"></span><font color="orange">  Logout</font></a></li>
+            <?php else: ?>
+            <li><a href="<?=APP_ROOT?>/users/login"><span class="glyphicon glyphicon-log-in logo-small"></span><font color="orange">  Login</font></a></li>
+            <li><a href="<?=APP_ROOT?>/users/register"><span class="glyphicon glyphicon-user logo-small"></span><font color="#9370db"> Sign up</font></font> </a></li>
+            <?php endif; ?>
+        </ul>
     </div>
 </nav>
-    <?php endif; ?>
+
 </header>
 
 <?php require_once('show-notify-messages.php'); ?>
