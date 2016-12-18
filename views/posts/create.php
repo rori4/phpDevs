1802d629ffe16b6be50987716518639d50b2f47e
Pec <peter.bratanov@gmail.com>
2016-12-18 18:04:24 +0200

    update

--- views/posts/create.php
+++ views/posts/create.php
@@ -7,7 +7,7 @@
         <input type="text" name="post_title" />
         <div>Content:</div>
         <textarea id="text_editor" name="post_content" rows="10"></textarea>
-        <div>
+        <div><br><br>
             <button type="submit" value="Create" class="btn btn-primary">Create</button>
             <button type="submit" value="Cancel" name="cancel-button" class="btn btn-primary"><a href="<?=APP_ROOT?>/posts">Cancel</a></button>
         </div>
