c0420f96a9ac1249aadc9db69147a18cb39dd3db
Pec <peter.bratanov@gmail.com>
2016-12-18 18:54:24 +0200

    update

--- views/posts/index.php
+++ views/posts/index.php
@@ -1,7 +1,5 @@
+<h1><?=htmlspecialchars($this->title)?></h1>
 
-
-<main>
-<form class="inline">
 <div class="middle">
 
     <table class="table table-hover">
@@ -11,35 +9,40 @@
             <th>ID</th>
             <th>Title</th>
             <th>Content</th>
-            <th>Date</th>
+<!--            <th>Date</th>-->
             <th>Author</th>
-            <th>Actions</th>
+            <th>Edit</th>
+            <th>Delete</th>
         </tr>
         <?php else: ?>
-        <div class="middle1"> <button type="submit" class="btn btn-primary" value="create"><a href="<?=APP_ROOT?>/posts/create">Create new post</a></button></div>
-
+        <div class="middle1">
+            <button type="submit" class="btn btn-primary" value="create"><a href="<?=APP_ROOT?>/posts/create">Create new post</a></button>
+        </div>
         <?php endif; ?>
-    <?php foreach ($this->posts as $post): ?>
+
         </thead>
         <tbody>
-        <tr>
-            <td><?=htmlspecialchars($post['id'])?></td>
-            <td><?=htmlspecialchars($post['title'])?></td>
-            <td><?=cutLongText($post['content'])?></td>
-            <td><?=htmlspecialchars($post['date'])?></td>
-            <td><?=htmlspecialchars($post['full_name'])?></td>
-            <td>
-                <div>
-                    <button type="button" class="btn btn-primary"><a href="<?=APP_ROOT?>/posts/edit/<?=htmlspecialchars($post['id'])?>">Edit</a></button>
-                    <button type="button" class="btn btn-primary"><a href="<?=APP_ROOT?>/posts/delete/<?=htmlspecialchars($post['id'])?>">Delete</a></button>
-                </div>
-            </td>
-        </tr>
-    <?php endforeach ?>
+            <?php foreach ($this->posts as $post): ?>
+                <tr>
+                    <td><?=htmlspecialchars($post['id'])?></td>
+                    <td><?=htmlspecialchars($post['title'])?></td>
+                    <td><?=cutLongText($post['content'])?></td>
+<!--                    <td>--><?//=htmlspecialchars($post['date'])?><!--</td>-->
+                    <td><?=htmlspecialchars($post['full_name'])?></td>
+                    <td>
+                        <div>
+                            <button type="button" class="btn btn-primary"><a href="<?=APP_ROOT?>/posts/edit/<?=htmlspecialchars($post['id'])?>">Edit</a></button>
+                        </div>
+                    </td>
+                    <td>
+                        <div>
+                            <button type="button" class="btn btn-primary"><a href="<?=APP_ROOT?>/posts/delete/<?=htmlspecialchars($post['id'])?>">Delete</a></button>
+                        </div>
+                    </td>
+                </tr>
+            <?php endforeach ?>
         </tbody>
     </table>
 
 </div>
 
-</form>
-</main>
--- views/users/index.php
+++ views/users/index.php
@@ -1,34 +1,39 @@
 <h1><?=htmlspecialchars($this->title)?></h1>
 
-<div class="middle">
-    <h2>Users</h2>
-    <table class="table table-hover">
-        <thead>
-        <tr>
-            <th>ID</th>
-            <th>Username</th>
-            <th>Fullname</th>
-            <?php if ($_SESSION['user_role']=="admin"):?>
-                <th>Actions</th>
-            <?php endif; ?>
-        </tr>
-        </thead>
-        <tbody>
-        <?php foreach ($this->users as $user) : ?>
+
+    <div class="middle1">
+        <table class="table table-hover">
+            <thead>
             <tr>
-                <td><?=$user['id']?></td>
-                <td><?=htmlspecialchars($user['username']) ?></td>
-                <td><?=htmlspecialchars($user['full_name']) ?></td>
+                <th>ID</th>
+                <th>Username</th>
+                <th>Fullname</th>
                 <?php if ($_SESSION['user_role']=="admin"):?>
-                    <td>
-                        <div>
-                            <button type="button" class="btn btn-primary"><a href="<?=APP_ROOT?>/users/edit/<?=htmlspecialchars($user['id'])?>">Edit</a></button>
-                            <button type="button" class="btn btn-primary"><a href="<?=APP_ROOT?>/users/delete/<?=htmlspecialchars($user['id'])?>">Delete</a></button>
-                        </div>
-                    </td>
+                    <th>Edit</th>
+                    <th>Delete</th>
                 <?php endif; ?>
             </tr>
-        <?php endforeach ?>
-        </tbody>
-    </table>
-</div>
+            </thead>
+            <tbody>
+                <?php foreach ($this->users as $user) : ?>
+                    <tr>
+                        <td><?=$user['id']?></td>
+                        <td><?=htmlspecialchars($user['username']) ?></td>
+                        <td><?=htmlspecialchars($user['full_name']) ?></td>
+                        <?php if ($_SESSION['user_role']=="admin"):?>
+                            <td>
+                                <div>
+                                    <button type="button" class="btn btn-primary"><a href="<?=APP_ROOT?>/users/edit/<?=htmlspecialchars($user['id'])?>">Edit</a></button>
+                                </div>
+                            </td>
+                            <td>
+                                <div>
+                                    <button type="button" class="btn btn-primary"><a href="<?=APP_ROOT?>/users/delete/<?=htmlspecialchars($user['id'])?>">Delete</a></button>
+                                </div>
+                            </td>
+                        <?php endif; ?>
+                    </tr>
+                <?php endforeach ?>
+            </tbody>
+        </table>
+    </div>
