7d8709a37cfd926161ba8e611360a06d3e2950b3
Pec <peter.bratanov@gmail.com>
2016-12-19 23:11:53 +0200

    updates on : user edit page post edit page delete posts css classes

--- views/users/delete.php
+++ views/users/delete.php
@@ -1,10 +1,11 @@
-<div class="middle">
+<div class="users-del">
     <?php $this->title = 'Delete User'; ?>
     <h1 style="text-align: center"><?=htmlspecialchars($this->title)?></h1>
 <h2>Are you sure you want to delete <?=htmlspecialchars($this->user['username'])?> with role of <?=htmlspecialchars($this->user['user_role'])?>?</h2>
-<form method="post">
-    <div class="middle1"><input type="submit" value="Edit" class="btn btn-primary" />
-        <button type="submit" value="Cancel" class="btn btn-primary"><a href="<?=APP_ROOT?>/posts">Cancel</a></button>
+<form method="post"><br>
+    <div>
+        <button type="submit" value="Delete" class="btn btn-primary">Delete </button>
+        <button type="submit" value="Cancel" class="btn btn-primary"><a href="<?=APP_ROOT?>/users">Cancel</a></button>
     </div>
 
 </form>
