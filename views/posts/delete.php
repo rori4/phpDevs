d8715bb2bc577d76708f94aa5ab7c1e6c32173ef
nstoilov99 <nstoilov99@gmail.com>
2016-12-19 22:54:04 +0200

    Delete posts section fixed and other minior bugs

--- views/posts/delete.php
+++ views/posts/delete.php
@@ -1,14 +1,14 @@
 <?php $this->title = 'Create New Post'; ?>
-<div class="middle">
+<div class="middle1">
     <h1><?=htmlspecialchars($this->title)?></h1>
 
-    <form method="post">
-        <div>Title:</div>
-        <input type="text" name="post_title" disabled value="<?=htmlspecialchars($this->post['title'])?>"/>
-        <div>Content:</div>
+    <form method="post" >
+        <div><b>Title:</b></div>
+        <input type="text" name="post_title" class="form-control" disabled value="<?=htmlspecialchars($this->post['title'])?>"/>
+        <div><b>Content:</b></div>
         <p><?=$this->post['content']?></p>
-        <div><input type="submit" value="Delete" />
-            <a href="<?=APP_ROOT?>/posts">[Cancel]</a></div>
+        <div><input type="submit" value="Delete" class="btn btn-primary" />
+            <button type="submit" value="Cancel" class="btn btn-primary"><a href="<?=APP_ROOT?>/posts">Cancel</a></button></div>
     </form>
 </div>
 
--- views/users/delete.php
+++ views/users/delete.php
@@ -4,7 +4,7 @@
 <h2>Are you sure you want to delete <?=htmlspecialchars($this->user['username'])?> with role of <?=htmlspecialchars($this->user['user_role'])?>?</h2>
 <form method="post">
     <div class="middle1"><input type="submit" value="Edit" class="btn btn-primary" />
-        <button type="submit" value="Create" class="btn btn-primary"><a href="<?=APP_ROOT?>/posts">Cancel</a></button>
+        <button type="submit" value="Cancel" class="btn btn-primary"><a href="<?=APP_ROOT?>/posts">Cancel</a></button>
     </div>
 
 </form>
