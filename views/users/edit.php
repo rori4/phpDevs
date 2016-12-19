e47d80876034f8a513e57d9d255e2bcca6e4230d
nstoilov99 <nstoilov99@gmail.com>
2016-12-19 22:36:21 +0200

    User edit form css fixed

--- views/users/edit.php
+++ views/users/edit.php
@@ -1,19 +1,19 @@
 <?php $this->title = 'Edit User'; ?>
 
-<div class="middle">
+<div class="middle1">
     <h1><?=htmlspecialchars($this->title)?></h1>
-    <form method="post">
+    <form method="post" class="form-inline">
         <div  class="form-group">
             <label for="Username">Username:</label>
-                 <input type="text"  class="form" name="username" value="<?=htmlspecialchars($this->user['username'])?>" /><br>
+                 <input type="text"  class="form-control"name="username" value="<?=htmlspecialchars($this->user['username'])?>" /><br>
             <label for="Full-Name">Full Name:</label>
-                 <input type="text" class="form" name="full_name" value="<?=htmlspecialchars($this->user['full_name'])?>" /><br><br>
+            <input type="text" class="form-control" name="full_name" value="<?=htmlspecialchars($this->user['full_name'])?>" /><br><br>
              <label for="Role">Role:</label>
-                 <select name="user_role">
+                 <select name="user_role" class="form-control">
                       <option value="admin">Admin</option>
                       <option value="user">User</option>
                  </select><br><br>
-        <input type="text" name="user_id" value="<?=htmlspecialchars($this->user['id'])?>" hidden />
+        <input type="text" name="user_id"  value="<?=htmlspecialchars($this->user['id'])?>" hidden />
         <div>
             <button type="submit" value="Edit" class="btn btn-primary">Edit: </button>
         </div>
