95ff3ef221c1a0aa30bdc35d76c1396e000e1c47
Pec <peter.bratanov@gmail.com>
2016-12-19 20:21:36 +0200

    user edit section update

--- views/users/edit.php
+++ views/users/edit.php
@@ -1,17 +1,22 @@
 <?php $this->title = 'Edit User'; ?>
 
-<h1><?=htmlspecialchars($this->title)?></h1>
-
-<form method="post">
-    <div>Username:</div>
-    <input type="text" name="username" value="<?=htmlspecialchars($this->user['username'])?>" />
-    <div>Full Name:</div>
-    <input type="text" name="full_name" value="<?=htmlspecialchars($this->user['full_name'])?>" />
-    <div>Role:</div>
-    <select name="user_role">
-        <option value="admin">Admin</option>
-        <option value="user">User</option>
-    </select>
-    <input type="text" name="user_id" value="<?=htmlspecialchars($this->user['id'])?>" hidden />
-    <div><input type="submit" value="Edit"></div>
-</form>
+<div class="middle">
+    <h1><?=htmlspecialchars($this->title)?></h1>
+    <form method="post">
+        <div  class="form-group">
+            <label for="Username">Username:</label>
+                 <input type="text"  class="form" name="username" value="<?=htmlspecialchars($this->user['username'])?>" /><br>
+            <label for="Full-Name">Full Name:</label>
+                 <input type="text" class="form" name="full_name" value="<?=htmlspecialchars($this->user['full_name'])?>" /><br><br>
+             <label for="Role">Role:</label>
+                 <select name="user_role">
+                      <option value="admin">Admin</option>
+                      <option value="user">User</option>
+                 </select><br><br>
+        <input type="text" name="user_id" value="<?=htmlspecialchars($this->user['id'])?>" hidden />
+        <div>
+            <button type="submit" value="Edit" class="btn btn-primary">Edit: </button>
+        </div>
+        </div>
+    </form>
+</div>
